44a0ef724a5916fe99de81e36a373ebac352eb05
Ailuro <ailuroluna.l@gmail.com>
2024-06-22 23:12:53 +0200

    create Punctuated equilibrium

--- epstein_network_civil_violence/agent.py
+++ epstein_network_civil_violence/agent.py
@@ -1,4 +1,6 @@
-from epstein_civil_violence.agent import Citizen, Cop 
+import math
+
+from epstein_civil_violence.agent import Citizen, Cop
 
 
 class Inhabitant(Citizen):
@@ -15,6 +17,7 @@ class Inhabitant(Citizen):
         vision,
         alpha,
         rumor_effect,
+        pretense = False,
     ):
         """
         Create a new Inhabitant.
@@ -51,6 +54,32 @@ class Inhabitant(Citizen):
         self.arrest_probability = None
         self.alpha = alpha
         self.rumor_effect = rumor_effect
+        self.pretense = pretense
+        self.cops_in_vision = 0
+        self.actives_in_vision = 0
+
+    def update_estimated_arrest_probability(self):
+        """
+        Based on the ratio of cops to actives in my neighborhood, estimate the
+        p(Arrest | I go active).
+        """
+        cops_in_vision = len([c for c in self.neighbors if c.breed == "cop"])
+        actives_in_vision = 1.0  # citizen counts herself
+        for c in self.neighbors:
+            if (
+                c.breed == "citizen"
+                and c.condition == "Active"
+                and c.jail_sentence == 0
+            ):
+                actives_in_vision += 1
+        print("###############")
+        print(self.pos)
+        print(actives_in_vision, cops_in_vision)
+        self.arrest_probability = 1 - math.exp(
+            -1 * self.model.arrest_prob_constant * (cops_in_vision / actives_in_vision)
+        )
+        self.cops_in_vision = cops_in_vision
+        self.actives_in_vision = actives_in_vision
 
     def step(self):
         """
@@ -61,11 +90,19 @@ class Inhabitant(Citizen):
             return  # no other changes or movements if agent is in jail.
         self.update_neighbors()
         self.update_estimated_arrest_probability()
+        print(self.arrest_probability)
         net_risk = self.risk_aversion * self.arrest_probability * self.jail_sentence ** self.alpha
-        if self.grievance - net_risk > self.threshold:
-            self.condition = "Active"
+        if self.condition == "Quiescent":
+            if self.grievance - net_risk > self.threshold and self.actives_in_vision >= (1 * self.cops_in_vision):
+            # if self.grievance - net_risk > self.threshold:
+                self.condition = "Active"
+            else:
+                self.condition = "Quiescent"
         else:
-            self.condition = "Quiescent"
+            if self.grievance - net_risk < self.threshold or self.cops_in_vision >= 1:
+                self.condition = "Quiescent"
+
+
         if self.model.movement and self.empty_neighbors:
             new_pos = self.random.choice(self.empty_neighbors)
             self.model.grid.move_agent(self, new_pos)
@@ -76,4 +113,27 @@ class Inhabitant(Citizen):
 
 
 class Police(Cop):
-    pass
+    def step(self):
+        """
+        Inspect local vision and arrest a random active agent. Move if
+        applicable.
+        """
+        self.update_neighbors()
+        active_neighbors = []
+        for agent in self.neighbors:
+            if (
+                    agent.breed == "citizen"
+                    and agent.condition == "Active"
+                    and agent.jail_sentence == 0
+            ):
+                active_neighbors.append(agent)
+        if active_neighbors:
+            arrestee = self.random.choice(active_neighbors)
+            # print(arrestee.arrest_probability)
+            if self.random.random()<arrestee.arrest_probability:
+                sentence = self.random.randint(0, self.model.max_jail_term)
+                arrestee.jail_sentence = sentence
+                arrestee.condition = "Quiescent"
+        if self.model.movement and self.empty_neighbors:
+            new_pos = self.random.choice(self.empty_neighbors)
+            self.model.grid.move_agent(self, new_pos)
